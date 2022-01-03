3d6d174a873441bd1ce977260a091344cfbea164
PDA <96939164+PDA-T@users.noreply.github.com>
2022-01-03 20:46:12 +0800

    Add files via upload

---
+++ Static/Admin/Js/Notice.js
@@ -0,0 +1,8 @@
+document.writeln("<p>此网站版权所有者:PDA。请勿盗版。</p>");
+document.writeln("<p>使用官方最新版本，提高建站体验和建站安全。</p>");
+document.writeln("<p>1.本系统为新版架构，有任何BUG请联系作者。</p>");
+document.writeln("<p>2.自适应所有设备，PC/手机/pad均可使用。</p>");
+document.writeln("<p>3.本系统不依托任何第三方CMS，纯PHP，对环境要求小，基本所有的PHP环境都可轻松带起。</p>");
+document.writeln("<p>4.所有广告及版权信息均可从后台更改。</p>");
+document.writeln("<p>无交流群，可通过邮箱联系作者</p>");
+document.writeln("<p>关注PDA，从快捷建站——引流变现，不懈努力！</p>");
